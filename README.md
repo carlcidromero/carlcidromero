64aea389ffe4edd5b84c76dd289eb33fd6b90e6a
Carl 'Cid' Romero <carlcidromero@gmail.com>
2021-09-20 19:41:49 -0700

    Complete 1st iteration of self-updating readme

--- README.md
+++ README.md
@@ -1,10 +1,8 @@
-- 👋 Hi, I’m @carlcidromero
-- 👀 I’m interested in people, music, video games, film, literature, technology.
-- 🌱 I’m primarily focused on learning React, secondarily on learning AWS, Java and Unix.
-- 💞️ I’m looking to collaborate on anything, preferably something strange.
-- 📫 How to reach me: carlcidromero@gmail.com
+Hi, I'm Carl (aka Cid).
+I currently reside in Graham, WA (US).
 
-<!---
-carlcidromero/carlcidromero is a ✨ special ✨ repository because its `README.md` (this file) appears on your GitHub profile.
-You can click the Preview link to take a look at your changes.
---->
+## Live Weather
+- clear sky
+- 65 degrees fahrenheit
+
+*This README was last refreshed Monday, September 20, 19:24 PDT.*
---
+++ index.js
@@ -0,0 +1,52 @@
+import dotenv from "dotenv";
+import fs from "fs";
+import Mustache from "mustache";
+import fetch from "node-fetch";
+
+dotenv.config();
+
+const MUSTACHE_MAIN_DIR = "./main.mustache";
+
+let DATA = {
+  name: "Carl (aka Cid)",
+  city: "Graham",
+  state: "WA",
+  country: "US",
+  date: new Date().toLocaleDateString("en-US", {
+    weekday: "long",
+    month: "long",
+    day: "numeric",
+    hour: "numeric",
+    minute: "numeric",
+    timeZoneName: "short",
+    timeZone: "America/Los_Angeles",
+    hour12: false,
+  }),
+};
+
+async function setWeatherInformation() {
+  await fetch(
+    `https://api.openweathermap.org/data/2.5/weather?q=Puyallup,US&appid=${process.env.OPEN_WEATHER_MAP_KEY}&units=imperial`
+  )
+    .then((r) => r.json())
+    .then((r) => {
+      DATA.temperature = Math.round(r.main.temp);
+      DATA.weather = r.weather[0].description;
+    });
+}
+
+async function generateReadMe() {
+  await fs.readFile(MUSTACHE_MAIN_DIR, (err, data) => {
+    if (err) throw err;
+
+    const output = Mustache.render(data.toString(), DATA);
+    fs.writeFileSync("README.md", output);
+  });
+}
+
+async function action() {
+  await setWeatherInformation();
+  await generateReadMe();
+}
+
+action();
